a6cd3e51090bc7436656fa392286c9a452a9e750
Lucas Costa <lucas6.silva@alunos.ifsuldeminas.edu.br>
2025-07-31 18:11:27 -0300

    fix to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Index from "./pages/Index";
 import Padroes from "./pages/Padroes";
 import PensamentoComputacional from "./pages/PensamentoComputacional";
@@ -12,21 +12,24 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/padroes" element={<Padroes />} />
-          <Route path="/pensamento-computacional" element={<PensamentoComputacional />} />
-          <Route path="/sobre" element={<Sobre />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/padroes" element={<Padroes />} />
+            <Route
+                path="/pensamento-computacional"
+                element={<PensamentoComputacional />}
+            />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </HashRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
 );
 
 export default App;
